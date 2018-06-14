d111d304a9fc2693cd640e7eb9867767b377f091
Luis Armando Gomez <luisgomez@MacBook-Pro.local>
2018-06-14 01:10:25 -0500

    Adding options json

---
+++ options.js
@@ -0,0 +1,14 @@
+module.exports = {
+	"site-details": {
+		"name": "Transit Pigeon"
+	},
+	"sections": [
+		{
+			"icon-image": "roundedTransitPigeon.png",
+			"order": 1,
+			"subtitle": "Find the nearest stop around you",
+			"title": "Transit Pigeon",
+			"layout-type": "half",
+		}
+	]
+}
