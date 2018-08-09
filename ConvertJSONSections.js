06fd2f2c52b9f6e97f91f9de69e4ec24c3639a06
Luis Armando Gomez <luisgomez@MacBook-Pro.local>
2018-08-09 00:54:26 -0400

    Adding content to each section's html

--- ConvertJSONSections.js
+++ ConvertJSONSections.js
@@ -12,6 +12,7 @@ module.exports = class ConvertJSONSections {
 	convertToHTML () {
 		let updatedHtml = '';
 		let headerContent = '';
+		let bodyContent = '';
 		
 		if(this.cssFiles){
 			this.cssFiles.forEach(file => {
@@ -19,42 +20,79 @@ module.exports = class ConvertJSONSections {
 			})
 		}
 		
-		if(this.sections){	
+		if (this.sections.length) {	
 			this.sections.forEach((section, index) => {
 				const isHero = section.order === 1 ? true : false;
 				const hasTitle = section.title !== '' ? true : false;
 				const hasSubTitle = section.subtitle !== '' ? true : false;
 				const hasAppIcon = section.iconImage !== '' ? true : false;
+				const imageIsLeft = section.iconImage && section.iconImagePlacement === 'left';
+				const imageIsRight = section.iconImage && section.iconImagePlacement === 'right';
 
-				updatedHtml += `
-					<html>
-						<head>
-							${headerContent !== '' ? headerContent : ''}
-						</head>
-						<body>
-							<section class="${isHero ? 'hero' : ''}">
-								<div class="${isHero ? 'hero-body container': 'container'}">
-									<div class="columns is-gapless">
-										<div class="column intro-text">
-											${hasTitle ? `<h1 class="${isHero ? 'hero-title': 'title'}">${section.title}</h1>` : ''}
-											${hasSubTitle ? `<h2 class="${isHero ? 'hero-subtitle' : 'subtitle'}">${section.subtitle}</h2>`: ''}
-											<br />
-
-										</div>
-										<div class="column intro-icon">
-											${hasAppIcon ? `<img src="./img/${section.iconImage}" width="300" height="300" />` : ''}
-										</div>
-									</div>
+				bodyContent += `
+					<section class="${isHero ? 'hero section' : ' section'}">
+						<div class="${isHero ? 'hero-body container': 'container'}">
+							<div class="columns is-gapless">
+								${
+									(imageIsLeft === true) ?
+									`<div class="column intro-icon">
+										${hasAppIcon ? `<img src="./img/${section.iconImage}" width="300" height="300" />` : ''}
+									</div>` : ''
+								}
+								<div class="column intro-text">
+									${hasTitle ? `<h1 class="${isHero ? 'hero-title': 'title'}">${section.title}</h1>` : ''}
+									${hasSubTitle ? `<h2 class="${isHero ? 'hero-subtitle' : 'subtitle'}">${section.subtitle}</h2>`: ''}
+									<br />
+									${
+										section.content ? this.generateSectionContent(section.content) : ''
+									}
 								</div>
-							</section>
-						</body>
-					</html>
+								${
+									imageIsRight === true ? 
+									`<div class="column intro-icon">
+										${hasAppIcon ? `<img src="./img/${section.iconImage}" width="300" height="300" />` : ''}
+									</div>` : ''
+								}
+							</div>
+						</div>
+					</section>
 				`;
 			});
 		}
 
+		updatedHtml += `
+			<html>
+				<head>
+					${headerContent !== '' ? headerContent : ''}
+				</head>
+				<body>
+					${bodyContent !== '' ? bodyContent : ''}
+				</body>
+			</html>
+		`;
+
 		this.htmlString = updatedHtml;
 
 		return this.htmlString
+	}
+
+	generateSectionContent (sectionContent) {
+		if(!sectionContent) {
+			return;
+		} else {
+			return sectionContent.reduce((accumulator, currentValue) => {
+				return (
+					accumulator +
+					`
+						<${currentValue.type && currentValue.type}>
+							${currentValue.content && currentValue.content}
+							${
+								currentValue.children && this.generateSectionContent(currentValue.children)	
+							}
+						</${currentValue.type && currentValue.type}>
+					`
+				)
+			}, '');
+		}
 	}
 }
--- options.js
+++ options.js
@@ -9,11 +9,135 @@ module.exports = {
 			"subtitle": "Find the nearest stop around you",
 			"title": "Transit Pigeon",
 			"layoutType": "half",
+		},
+		{
+			"iconImage": "iphone-white-landing.png",
+			"iconImagePlacement": "left",
+			"order": 2,
+			"subtitle": "",
+			"title": "A modern design with a clear and simple interface",
+			"content": [
+				{
+					"type":"ul",
+					"children": [
+						{
+							"type": "li",
+							"content": 'View times for many upcoming trains and buses'
+						},
+						{
+							"type": "li",
+							"content": 'Toggle each stop\'s direction and view incoming or outgoing times'
+						},
+						{
+							"type": "li",
+							"content": 'Pull to refresh all of your favorite stops'
+						}
+					],
+					"content": ''
+				},
+				{
+					"type": "p",
+					"content": "this is general content for the p tag - working?"
+				}
+			]
+		},
+		{
+			"iconImage": "iphone-white-search.png",
+			"iconImagePlacement": "right",
+			"order": 3,
+			"subtitle": "",
+			"title": "View all the stop times near you with one tap",
+			"content": [
+				{
+					"type": "ul",
+					"children": [
+						{
+							"type": "li",
+							"content": "Transit Pigeon will display incoming and outgoing times for both train and bus stops around you"
+						},
+						{
+							"type": "li",
+							"content": "Want to view stops for a different neighborhood? Use the search feature to find stop times in that area"
+						}
+					],
+					content: ''
+				}
+			]
+		},
+		{
+			"iconImage": 'iphone-white-detail.png',
+			"iconImagePlacement": "left",
+			"order": 4,
+			"subtitle": "",
+			"title": "Turn by turn directions are a tap away",
+			"content": [
+				{
+					"type": "ul",
+					"children": [
+						{
+							"type": "li",
+							"content": "Walk and drive directions to stops are one click away"
+						},
+						{
+							"type": "li",
+							"content": "Pressing the directions button will automatically load Apple maps with the correct destination"
+						}
+					],
+					content: ""
+				}
+			]
+		},
+		{
+			"iconImage": "iphone-white-login.png",
+			"iconImagePlacement": "right",
+			"order": 5,
+			"subtitle": "",
+			"title": "Save your favorites to the cloud",
+			"content": [
+				{
+					"type": "ul",
+					"children": [
+						{
+							"type": "li",
+							"content": "Stops are not saved on your phone"
+						},
+						{
+							"type": "li",
+							"content": "Login to Transit Pigeon on any iPhone to load your favorite stops"
+						}
+					],
+					content: ""
+				}
+			]
+		},
+		{
+			"iconImage": "iphone-white-list.png",
+			"iconImagePlacement": "left",
+			"order": 6,
+			"subtitle": "",
+			"title": "CTA Busses and CTA Trains",
+			"content":[
+				{
+					"type": "ul",
+					"children": [
+						{
+							"type": "li",
+							"content": "Metra *coming soon"
+						}
+					],
+					content: ""
+				}
+			]
 		}
 	],
 	"images": [
 		{directory: '/dist/img/', name: 'hero', url: 'https://s3.amazonaws.com/public-gomez-aws/img/hero.png', type: 'png'},
-		{directory: '/dist/img/', name: 'roundedTransitPigeon', url: 'https://s3.amazonaws.com/public-gomez-aws/img/roundedTransitPigeon.png', type: 'png'}
+		{directory: '/dist/img/', name: 'roundedTransitPigeon', url: 'https://s3.amazonaws.com/public-gomez-aws/img/roundedTransitPigeon.png', type: 'png'},
+		{directory: '/dist/img/', name: 'iphone-white-landing', url: 'https://s3.amazonaws.com/public-gomez-aws/img/iphone-6-images/iphone-white-landing.png', type: 'png'},
+		{directory: '/dist/img/', name: 'iphone-white-search', url: 'https://s3.amazonaws.com/public-gomez-aws/img/iphone-6-images/iphone-white-search.png', type: 'png'},
+		{directory: '/dist/img/', name: 'iphone-white-detail', url: 'https://s3.amazonaws.com/public-gomez-aws/img/iphone-6-images/iphone-white-detail.png', type: 'png'},
+		{directory: '/dist/img/', name: 'iphone-white-login', url: 'https://s3.amazonaws.com/public-gomez-aws/img/iphone-6-images/iphone-white-login.png', type: 'png'},
+		{directory: '/dist/img/', name: 'iphone-white-list', url: 'https://s3.amazonaws.com/public-gomez-aws/img/iphone-6-images/iphone-white-list.png', type: 'png'}
 	],
 	'published-platforms': ['ios'],
 	"css": [
